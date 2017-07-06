0c4864da70fda5fe475b1211ab6a4a03242b0be3
Huangerb <bradley.c.yu@gmail.com>
2017-07-06 08:42:42 -0500

    Initial finish

--- Interest Calculator/SImpleSecondViewController.swift
+++ Interest Calculator/SImpleSecondViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class SImpleSecondViewController: UIViewController {
     @IBOutlet weak var answerDisplayed: UILabel!
+    @IBOutlet weak var totalDisplayed: UILabel!
+    
     var totalInterestDisplayed: Double!
     var answerInterest: String!
+    var totalMoneyDisplayed: Double!
+    var answerMoney: String!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        answerInterest = String(totalInterestDisplayed)
-        
-        answerDisplayed.text = "The interest you will pay will be: $" + answerInterest
+        answerInterest = String(format: "%.2f", totalInterestDisplayed)
+        answerDisplayed.text = "The interest you will be: $" + answerInterest
+        answerMoney = String(format: "%.2f", totalMoneyDisplayed)
+        totalDisplayed.text = "The total will be: $" + answerMoney
     }
 
     override func didReceiveMemoryWarning() {
